9f921f57dd538f6aee29b8d83c603a746906e3c9
Phillip Ruhesi <pruhesi@gmail.com>
2018-05-25 19:26:56 +0200

    Added check for if page size is 0 to show All
    Added check for if page size is 0 to show All

--- src/PageSizeSelect.vue
+++ src/PageSizeSelect.vue
@@ -2,7 +2,10 @@
   <label name="PageSizeSelect">
     <select class="form-control input-sm -page-size-select" v-model="query.limit"
       @change="query.offset = 0 /* reset to the first page */">
-      <option v-for="i in pageSizeOptions" :value="i">{{ i }}</option>
+      <option v-for="i in pageSizeOptions" :value="i">
+        <span v-if="i===0">{{'All'}}</span>
+        <span v-if="i!==0">{{i}}</span>
+       </option>
     </select>
     {{ $i18nForDatatable('items / page') }}
   </label>
